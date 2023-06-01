3992b326f15c3a68591c2660b1a92212d2725942
Rushikesh Tarapure <rushikeshtarapure@gofynd.com>
2023-06-02 00:05:41 +0530

    Removed default code also router

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,9 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 import Home from "./components/Home.vue"
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-      <Home />
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+<Home />
 
   <RouterView />
 </template>
