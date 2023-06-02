fe195645b300edb956091e7922b14c165755f90c
Rushikesh Tarapure <rushikeshtarapure@gofynd.com>
2023-06-02 20:51:52 +0530

    created task and create task component

---
+++ src/components/CreateTask.vue
@@ -0,0 +1,28 @@
+<template>
+<div>Task</div>
+  <input type="text" placeholder="Add task">
+  <div>Day and time</div>
+  <input type="text" placeholder="Add Day and Time">
+  <div>Set a remainder
+  <input type="checkbox" name="remainder" id="remainder"></div>
+<Button title="Save Task"></Button>
+</template>
+
+<script>
+import Button from './Button.vue'
+
+export default {
+  components: { Button },
+name : "CreateTasks",
+props : { 
+    tasks : { 
+type : Array,
+default : [],
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,21 @@
+<template>
+  <div :key="task.id" v-for="task in tasks">
+    <h2>{{ task.text }}</h2>
+    <p>{{ task.day }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+name : "Tasks",
+props : { 
+    tasks : { 
+type : Array,
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
