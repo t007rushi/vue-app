39445461f4cdeea4bf792e70f0b11041d6b46d5d
Rushikesh Tarapure <rushikeshtarapure@gofynd.com>
2023-06-04 21:29:27 +0530

    create task component

---
+++ src/components/Task.vue
@@ -0,0 +1,26 @@
+<template>
+   <h2>{{ task.text }}
+      <i @click="DeleteItem(task.id)" class="fas fa-times"></i>
+    </h2>
+    <p>{{ task.day }}</p>
+</template>
+
+<script>
+export default {
+  props: {
+    task: {
+      type: Object,
+    }
+  },
+    methods : {
+DeleteItem (id)  {
+this.$emit('delete-task', id)
+}
+  },
+  emits : ['delete-task']
+}
+</script>
+
+<style>
+
+</style>
